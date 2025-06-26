78f48c5546b6ff12e4544ec5e26eb984870826c9
Vijitverm1234 <143703788+Vijitverm1234@users.noreply.github.com>
2025-06-26 20:37:07 +0530

    added all Loop related concepts

--- Linked List/Day2 medium level question/Loop_Detection_length.cpp
+++ Linked List/Day2 medium level question/Loop_Detection_length.cpp
@@ -7,7 +7,56 @@ typedef struct Node
     Node *next;
 } Node;
 
-
+bool detectLoop(Node* head){
+  Node* temp=head;
+  Node* slow=head;
+  Node* fast=head;
+  while(fast && fast->next){
+    slow=slow->next;
+    fast=fast->next->next;
+    if(slow==fast){
+        return true;
+    }
+  }
+  return false;
+}
+Node* getLoopHead(Node* head){
+  Node* temp=head;
+  Node* slow=head;
+  Node* fast=head;
+  while(fast && fast->next){
+    slow=slow->next;
+    fast=fast->next->next;
+    if(slow==fast){
+        slow=head;
+        while(slow!=fast){
+            slow=slow->next;
+            fast=fast->next;
+        }
+        return slow;
+    }
+  }
+  return NULL;
+}
+int getLooplength(Node* head){
+  Node* temp=head;
+  Node* slow=head;
+  Node* fast=head;
+  while(fast && fast->next){
+    slow=slow->next;
+    fast=fast->next->next;
+    if(slow==fast){
+        int count=1;
+        fast=fast->next;
+        while(slow!=fast){
+            count++;
+            fast=fast->next;
+        }
+        return count;
+    }
+  }
+  return 0;
+}
 void printList(Node* head) {
     Node* current = head;
     while (current) {
@@ -53,13 +102,27 @@ void printList(Node* head) {
 
     cout << "\nEntered linked list: ";
     Node *temp2 = head;
+    Node * temp3=NULL;
     while (temp2 != NULL)
     {
         cout << temp2->data << " ";
+        temp3=temp2;
         temp2 = temp2->next;
     }
     cout << endl;
-    
+    temp3->next=head;
+    bool loop=detectLoop(head);
+    if(loop){
+        cout<<"\nloop detected";
+        Node* loophead=getLoopHead(head);
+        if(loophead){
+            cout<<"\nLoop head : "<<loophead->data;
+        }
+        cout<<"\nLoop Length : "<<getLooplength(head);
+    }
+    else{
+        cout<<"\n no loop in linked list";
+    }
     
     return 0;
 }
