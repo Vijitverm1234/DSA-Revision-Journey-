c62b8a6726ca82c60834de8d8a0d6fccbc07a03b
Vijitverm1234 <143703788+Vijitverm1234@users.noreply.github.com>
2025-07-09 10:40:48 +0530

    Tree

--- Tree/height , depth , radius/isbalanced.cpp
+++ Tree/height , depth , radius/isbalanced.cpp
@@ -8,18 +8,20 @@ Node* left;
 Node* right;
 Node(int val) : val(val), left(nullptr), right(nullptr) {}
 }Node;
-int dia=0;
+
 int solve(Node* root){
     if(root==NULL){
         return 0;
     }
     int left=solve(root->left);
     int right=solve(root->right);
-    dia=max(dia,left+right);
+    if(abs(left-right)>1){
+        return -1;
+    }
     return 1+max(left,right);
 }
 int main(){
-    Node* root = new Node(1);
+       Node* root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
     root->left->left = new Node(4);
@@ -27,6 +29,10 @@ int main(){
     root->left->right->right = new Node(6);
     root->left->right->right->right = new Node(7);
     int depth=solve(root);
-    cout<<"depth : "<<depth<<endl;
-    cout<<"Diameter : "<<dia<<endl; 
+    if(depth==-1){
+        cout<<"Not Balanced";
+    }
+    else{
+        cout<<"Balanced";
+    }
 }
