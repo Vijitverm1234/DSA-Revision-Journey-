a705b4769bd8dffc969b6620d1e63467f753ef84
Vijit Verma <143703788+Vijitverm1234@users.noreply.github.com>
2025-11-22 09:13:21 +0000

    feat(tree): implement flattening of a binary tree

---
+++ Tree/hard/Flatten_BT.cpp
@@ -0,0 +1,17 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    Node * prev=NULL;
+    void flatten(Node *node) {
+        // code here
+        if(node==NULL){
+            return;
+        }
+        flatten(node->right);
+        flatten(node->left);
+        node->right=prev;
+        node->left=NULL;
+        prev=node;
+    }
+};
